3b4ac977efb2b4830ce185382cd0008fb6964ae7
Magno Moreira <magno.moreira@pagar.me>
2021-10-03 15:08:17 -0300

    Commit do curso da alura para estudar no pc de casa

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult Adicionar([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+           
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilme()
+        {
+            return Ok( filmes) ;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id )
+        {
+           var result =  filmes.FirstOrDefault(filme => filme.Id == id);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O compo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required]
+        public string Direto { get; set; }
+        [StringLength(30,ErrorMessage = "Não pode ultrapassar 30 caracteres")]
+        public string  Genero { get; set; }
+
+        [Range(1, 600)]
+        public int Duracao { get; set; }
+    }
+}
